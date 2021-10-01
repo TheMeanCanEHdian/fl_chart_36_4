866403633b2788d2b8593a40343c199f365b2750
TheMeanCanEHdian <drivard100@gmail.com>
2021-10-01 09:39:12 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fl_chart_36_4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fl_chart_36_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/fl_chart_swipe_issue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fl_chart_swipe_issue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
